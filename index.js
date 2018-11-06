f636fa932115deb42893081aee79feaecf2baba3
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-06 23:40:20 +0000

    Form now sends post request to API

--- index.js
+++ index.js
@@ -17,16 +17,33 @@ app.set('view engine', 'handlebars')
 
 const port = 8080
 
+const request = require('request')
+
 app.get('/', async(req, res) => {
 	res.render('home')
 })
 
-app.get('/create_article', async(req, res) => {
+app.get('/create_article', (req, res) => {
 	res.render('create_article')
+})
+
+app.get('/articles/', (req, res) => {
+	res.render('articles')
 })
 
 app.post('/articles', async(req, res) => {
 	console.log(req.body)
+
+	let newArticle = JSON.stringify(req.body)
+
+	request.post({
+		headers: {'content-type' : 'application/json'},
+		url: 'http://localhost:8081/api/v1.0/articles',
+		body: newArticle}, () => {
+			console.log("POST request sent to API")
+		})
+
+	res.redirect('/')
 })
 
 app.get('/create_event', async(req, res) => {
@@ -41,9 +58,6 @@ app.get('/register', async(req, res) => {
 	res.render('register')
 })
 
-app.get('/articles/', (req, res) => {
-	res.render('articles')
-})
 
 app.get('/articles/:id', (req, res) => {
 	console.log(req.params.id)
