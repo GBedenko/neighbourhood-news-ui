773764ef914f00b79874aeca9e4d29b04a542b21
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-06 22:17:59 +0000

    Added article page which links to backend api

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const app = express()
 
 const handlebars = require('express-handlebars').create({defaultLayout: 'main'})
 const bodyParser = require('body-parser')
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
 const port = 8080
+
+const request = require('request');
 
 app.get('/', async(req, res) => {
 	res.render('home')
@@ -23,9 +25,19 @@ app.get('/search', async(req, res) => {
 	res.render('search')
 })
 
+app.get('/articles', (req, res) => {
+
+	request('http://localhost:8081/api/v1.0/articles', (error, response, body) => {
+
+		const articlesJSON = JSON.parse(body)
+
+		res.render('articles', {articles: articlesJSON})
+	})
+})
+
 app.get('/articles/:id', (req, res) => {
-	console.log(req.params.id)
-	res.render('article')
+
+	res.redirect('/articles')
 })
 
 app.get('/form', async(req, res) => res.render('form'))
