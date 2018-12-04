94eebb439dc9b31d4053ee8af50f399566432464
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-12-04 00:01:08 +0000

    Demo run alongside acceptance tests which are run in background at same time

---
+++ __tests__/acceptance-tests-with-demo.js
@@ -0,0 +1,195 @@
+'use strict'
+
+const puppeteer = require('puppeteer')
+
+let page
+let browser
+
+describe('Demo of Navigating through Web Application', () => {
+
+    beforeAll( async() => {
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 40,        
+            args: [`--window-size=${2220},${1080}`, '--disable-http2']
+        })
+        let pages = await browser.pages()
+        page = pages[0]
+        await page.setViewport({ width: 1850, height: 1080});
+    })
+    
+    afterAll( async() => {
+        await page.close()
+        await browser.close()
+    })
+
+	test('Opening the webpage directs user to the login/register page', async done => {
+        
+        // Go to welcome page
+        await page.goto('http://localhost:8080', { waitUntil: 'domcontentloaded' })
+
+        expect(page.url()).toBe('http://localhost:8080/')
+
+        done()
+
+    }, 5000)
+
+	test('Logging in directs user to the all posts page', async done => {
+
+        // Enter login details and log in
+        await page.waitForSelector('input[name=email]')
+        await page.click('input[name=username]')
+        await page.type('input[name=username]', 'Test1')
+        await page.waitFor(1000)
+        await page.type('input[name=password]', 'test')
+        await page.waitFor(1000)
+        await page.keyboard.press('Enter');
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/all_posts')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view articles only page', async done => {
+
+        // Go to articles only        
+        await page.waitForSelector('a[id=articles]')
+        await page.click('a[id=articles]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/articles')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view events only page', async done => {
+
+        // Go to events only
+        await page.waitForSelector('a[id=events]')
+        await page.click('a[id=events]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/events')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view articles sorted by highest page', async done => {
+
+        // Go to articles highest rated
+        await page.waitForSelector('a[id=articles_highest_rated]')
+        await page.click('a[id=articles_highest_rated]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/articles?sort=highest_rated')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view articles sorted by lowest page', async done => {
+
+        // Go to articles lowest rated        
+        await page.waitForSelector('a[id=articles_lowest_rated]')
+        await page.click('a[id=articles_lowest_rated]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/articles?sort=lowest_rated')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view events sorted by highest page', async done => {
+
+        // Go to events highest rated
+        await page.waitForSelector('a[id=events_highest_rated]')
+        await page.click('a[id=events_highest_rated]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/events?sort=highest_rated')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view events sorted by lowest page', async done => {
+
+        // Go to events lowest rated
+        await page.waitForSelector('a[id=events_lowest_rated]')
+        await page.click('a[id=events_lowest_rated]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/events?sort=lowest_rated')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to admin dashboard page', async done => {
+
+        // Go to admin dashboard
+        await page.waitForSelector('a[id=admin_dashboard]')
+        await page.click('a[id=admin_dashboard]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/admin_dashboard')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to create an article page', async done => {
+
+        // Go to create article
+        await page.waitForSelector('a[id=create_article]')
+        await page.click('a[id=create_article]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/create_article')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to create an event page', async done => {
+
+        // Go to create event
+        await page.waitForSelector('a[id=create_event]')
+        await page.click('a[id=create_event]')
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/create_event')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view an article page', async done => {
+
+        // View an article
+        await page.goto('http://localhost:8080/articles/5bf45d17a86fb42417eb7792', { waitUntil: 'domcontentloaded' })
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/articles/5bf45d17a86fb42417eb7792')
+
+        done()
+
+    }, 6000)
+
+	test('Navigating to view an event page', async done => {
+
+        // View an event
+        await page.goto('http://localhost:8080/events/5bf27fe84212f00c80d5011a', { waitUntil: 'domcontentloaded' })
+
+        await page.waitFor(1000)
+        expect(page.url()).toBe('http://localhost:8080/events/5bf27fe84212f00c80d5011a')
+
+        done()
+
+    }, 6000)
+})
--- __tests__/acceptance-tests.js
+++ __tests__/acceptance-tests.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer')
 let page
 let browser
 
-beforeEach( async() => {
+beforeAll( async() => {
 	browser = await puppeteer.launch({
 		headless: true,
         slowMo: 40,        
@@ -20,125 +20,125 @@ afterAll( async() => {
 	await browser.close()
 })
 
-describe('View Webpages', () => {
+describe('Webpages appear correctly', () => {
 
-	test('Can view all posts', async done => {
-		await page.waitFor(1000)
+	test('View all posts displays correctly', async done => {
+
 		await page.goto('http://localhost:8080/all_posts', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+
 		await page.screenshot({ path: 'screenshots/all_posts.png', fullPage: true })
+
         const title = await page.title()
-        
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view articles list', async done => {
-		await page.waitFor(1000)
+	test('View articles list displays correctly', async done => {
+
 		await page.goto('http://localhost:8080/articles', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+
 		await page.screenshot({ path: 'screenshots/articles.png', fullPage: true })
-        const title = await page.title()
-        
+
+        const title = await page.title()        
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view events list', async done => {
-		await page.waitFor(1000)
+	test('View events list displays correctly', async done => {
+		
 		await page.goto('http://localhost:8080/events', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+		
 		await page.screenshot({ path: 'screenshots/events.png', fullPage: true })
-        const title = await page.title()
-        
+
+        const title = await page.title()        
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view an article', async done => {
-		await page.waitFor(1000)
+	test('View an article displays correctly', async done => {
+		
 		await page.goto('http://localhost:8080/articles/5bf45d17a86fb42417eb7792', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+		
 		await page.screenshot({ path: 'screenshots/article.png', fullPage: true })
+
 		const title = await page.title()
-		
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view an event', async done => {
-		await page.waitFor(1000)
+	test('View an event displays correctly', async done => {
+		
 		await page.goto('http://localhost:8080/events/5bf27fe84212f00c80d5011a', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+		
 		await page.screenshot({ path: 'screenshots/event.png', fullPage: true })
-		const title = await page.title()
-		
+
+		const title = await page.title()		
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view create article form', async done => {
-		await page.waitFor(1000)
+	test('View create article form displays correctly', async done => {
+		
 		await page.goto('http://localhost:8080/create_article', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+		
 		await page.screenshot({ path: 'screenshots/create_article.png', fullPage: true })
-		const title = await page.title()
-		
+
+		const title = await page.title()		
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view create event form', async done => {
-		await page.waitFor(1000)
+	test('View create event form displays correctly', async done => {
+		
 		await page.goto('http://localhost:8080/create_event', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+		
 		await page.screenshot({ path: 'screenshots/create_event.png', fullPage: true })
-		const title = await page.title()
-		
+
+		const title = await page.title()		
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view admin dashboard', async done => {
-		await page.waitFor(1000)
+	test('View admin dashboard displays correctly', async done => {
+		
 		await page.goto('http://localhost:8080/admin_dashboard', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+		
 		await page.screenshot({ path: 'screenshots/admin_dashboard.png', fullPage: true })
-		const title = await page.title()
-		
+
+		const title = await page.title()		
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view user page', async done => {
-		await page.waitFor(1000)
+	test('View user page displays correctly', async done => {
+		
 		await page.goto('http://localhost:8080/users/5bf49a2c9cf07a6751732c49', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
+
 		await page.screenshot({ path: 'screenshots/user.png', fullPage: true })
-		const title = await page.title()
-		
+
+		const title = await page.title()		
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 
-	test('Can view welcome page', async done => {
-		await page.waitFor(1000)
-		await page.goto('http://localhost:8080/', { waitUntil: 'domcontentloaded' })
-		await page.waitFor(1000)
-		await page.screenshot({ path: 'screenshots/welcome.png', fullPage: true })
-		const title = await page.title()
+	test('View welcome page displays correctly', async done => {
 		
+		await page.goto('http://localhost:8080/', { waitUntil: 'domcontentloaded' })
+		
+		await page.screenshot({ path: 'screenshots/welcome.png', fullPage: true })
+
+		const title = await page.title()		
 		expect(title).toBe('Local News Application - Genaro Bedenko')
-		await browser.close()
+		
 		done()
 	})
 })
@@ -148,7 +148,6 @@ describe('Logging in to the application', () => {
 	test('Logging in with correct credentials redirects to all_posts page', async done => {
 
 		// Go to welcome page
-		await page.waitFor(1000)
 		await page.goto('http://localhost:8080', { waitUntil: 'domcontentloaded' })
 
 		// Enter login details and log in
